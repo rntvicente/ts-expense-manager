ce3ff6646b1d341f59eb4f449c8502e15fd6c576
Renato Vicente <rntvicente@gmail.com>
2023-03-11 17:51:20 -0300

    feat: camada de infra

--- src/infra/repository/user-repository-database.ts
+++ src/infra/repository/user-repository-database.ts
@@ -1,19 +1,20 @@
 import { MongoHelper } from '../database/helper-mongodb';
-import {
-  UserModel,
-  UserRepository
-} from '../../application/repositories/user-repository';
-import { UniqueEntityIdVO } from '../../shared/value-object/unique-entity-id-vo';
+import { UserMapper } from '../mappers/user-mapper';
+
+import { UserRepository } from '../../application/repositories/user-repository';
+import { User } from '../../domains/users/user-entity';
 
 const COLLECTION_NAME = 'user';
 
 export class UserRepositoryDatabase implements UserRepository {
   constructor(private readonly database: MongoHelper) {}
 
-  async save(user: UserModel): Promise<UniqueEntityIdVO> {
-    const collection = await this.database.getCollection(COLLECTION_NAME);
-    const { insertedId } = await collection.insertOne(user);
+  async save(user: User): Promise<string> {
+    const model = UserMapper.toModel(user);
 
-    return new UniqueEntityIdVO(insertedId.toString());
+    const collection = await this.database.getCollection(COLLECTION_NAME);
+    const { insertedId } = await collection.insertOne(model);
+
+    return insertedId.toString();
   }
 }
