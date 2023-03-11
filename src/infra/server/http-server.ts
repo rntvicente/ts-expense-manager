ee7e8e7fa7ce2aca2fb4cd2398cb063349a2053e
Renato Vicente <rntvicente@gmail.com>
2023-03-11 20:08:46 -0300

    refactor: removendo promise

--- src/infra/server/http-server.ts
+++ src/infra/server/http-server.ts
@@ -1,4 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-types */
 export interface HttpServer {
-  on (method: string, url: string, callback: Function): void;
-  listen (port: number): Promise<void>;
+  on(method: string, url: string, callback: Function): void;
+  listen(port: number): void;
 }
