cee07114b99848d4f1c9db1940610509aefeb092
Renato Vicente <rntvicente@gmail.com>
2023-03-11 17:49:37 -0300

    feat: adicionando password

--- src/domains/users/user-entity.ts
+++ src/domains/users/user-entity.ts
@@ -1,7 +1,6 @@
 import { MissingParamError } from '../../shared/errors/missing-param-error';
 
 import { EmailVO } from '../../shared/value-object/email-vo';
-import { PasswordVO } from '../../shared/value-object/password-vo';
 import { UniqueEntityIdVO } from '../../shared/value-object/unique-entity-id-vo';
 
 export class User {
@@ -9,20 +8,20 @@ export class User {
   private readonly _firstName: string;
   private readonly _lastName: string;
   private readonly _email: EmailVO;
-  private readonly _password: PasswordVO;
+  private readonly _password: string;
 
   constructor(
-    id: string | undefined,
     firstName: string,
     lastName: string,
     email: string,
-    password: string
+    password: string,
+    id?: string | undefined
   ) {
     this._id = new UniqueEntityIdVO(id);
     this._firstName = firstName;
     this._lastName = lastName;
     this._email = new EmailVO(email);
-    this._password = new PasswordVO(password, 'AQUI POSSO APLICAR O ENCRYPTER DA QUE ESTÀ EM MINHA INFRA');
+    this._password = password;
 
     this.validate();
   }
@@ -30,14 +29,27 @@ export class User {
   private validate() {
     if (!this._firstName) throw new MissingParamError('First Name');
     if (!this._lastName) throw new MissingParamError('Last Name');
+    if (!this._password) throw new MissingParamError('Password');
   }
 
   get id() {
     return this._id;
   }
 
+  get firstname() {
+    return this._firstName;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+
   get email() {
     return this._email.value;
+  }
+
+  get password() {
+    return this._password;
   }
 
   get fullName() {
