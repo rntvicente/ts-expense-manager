c2c06f3bd57053c2ee7cca1962b01bcf957f1ff8
Renato Vicente <rntvicente@gmail.com>
2023-03-11 17:50:01 -0300

    test: deve lançar erro quando não informado password

--- src/domains/users/user-entity.spec.ts
+++ src/domains/users/user-entity.spec.ts
@@ -10,46 +10,46 @@ describe('# Entity User Test Unit', () => {
   const firstName = chance.first({ nationality: 'it' });
   const lastName = chance.last({ nationality: '*' });
   const email = chance.email();
+  const password = chance.hash();
 
   it('should throw error when not informe firstname', () => {
-    expect(() => new User(id, '', lastName, email)).toThrow(
+    expect(() => new User('', lastName, email, password, id)).toThrow(
       new Error('Missing Param: First Name')
     );
   });
 
   it('should throw error when not informe lastname', () => {
-    expect(() => new User(id, firstName, '', email)).toThrow(
+    expect(() => new User(firstName, '', email, password, id)).toThrow(
       new Error('Missing Param: Last Name')
     );
   });
 
   it('should throw error when not informe email', () => {
-    expect(() => new User(id, firstName, lastName, '')).toThrow(
+    expect(() => new User(firstName, lastName, '', password, id)).toThrow(
       new Error('Invalid Field: E-mail')
     );
   });
 
+  it('should throw error when not informe password', () => {
+    expect(() => new User(firstName, lastName, email, '', id)).toThrow(
+      new Error('Missing Param: Password')
+    );
+  });
+
   it('should create User with Unique Entity ID', () => {
-    const user = new User('', firstName, lastName, email);
+    const user = new User(firstName, lastName, email, password);
 
     expect(user.id).toBeInstanceOf(UniqueEntityIdVO);
   });
 
   it('should create User when informed id', () => {
-    const user = new User(id, firstName, lastName, email);
+    const user = new User(firstName, lastName, email, password, id);
 
     expect(user.id.toString()).toStrictEqual(id);
-  });
-
-  it('should get email when create User', () => {
-    const user = new User(id, firstName, lastName, email);
-
+    expect(user.firstname).toStrictEqual(firstName);
+    expect(user.lastName).toStrictEqual(lastName);
+    expect(user.password).toStrictEqual(password);
     expect(user.email).toStrictEqual(email);
-  });
-
-  it('should get fullName when create User', () => {
-    const user = new User(id, firstName, lastName, email);
-
     expect(user.fullName).toStrictEqual(`${firstName} ${lastName}`);
   });
 });
