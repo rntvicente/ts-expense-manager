123fea0a5810e070bc8ddaa55d2524b5e2ac2867
Renato Vicente <rntvicente@gmail.com>
2023-03-12 20:13:50 -0300

    feat: metodo findOne

--- src/application/repositories/user-repository.ts
+++ src/application/repositories/user-repository.ts
@@ -1,5 +1,12 @@
+import { Document, Filter, FindOptions } from 'mongodb';
+
+import { UserModel } from '../../infra/models/user-model';
 import { User } from '../../domains/users/user-entity';
 
 export interface UserRepository {
   save(user: User): Promise<string>;
+  findOne(
+    filter: Filter<Document>,
+    optins: FindOptions<Document>
+  ): Promise<UserModel | null>;
 }
