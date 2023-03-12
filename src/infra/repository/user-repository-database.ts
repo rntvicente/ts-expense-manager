e7f0102d1f7003dc49c94c824e994cc748966522
Renato Vicente <rntvicente@gmail.com>
2023-03-12 20:14:07 -0300

    feat: adapter de User FindOne

--- src/infra/repository/user-repository-database.ts
+++ src/infra/repository/user-repository-database.ts
@@ -1,13 +1,24 @@
 import { MongoHelper } from '../database/helper-mongodb';
+import { Filter, Document, FindOptions } from 'mongodb';
+
 import { UserMapper } from '../mappers/user-mapper';
 
 import { UserRepository } from '../../application/repositories/user-repository';
 import { User } from '../../domains/users/user-entity';
+import { UserModel } from '../models/user-model';
 
 const COLLECTION_NAME = 'users';
 
 export class UserRepositoryDatabase implements UserRepository {
   constructor(private readonly database: MongoHelper) {}
+
+  async findOne(
+    filter: Filter<Document>,
+    optins: FindOptions<Document>
+  ): Promise<UserModel | null> {
+    const collection = await this.database.getCollection(COLLECTION_NAME);
+    return await collection.findOne<UserModel | null>(filter, optins);
+  }
 
   async save(user: User): Promise<string> {
     const model = UserMapper.toModel(user);
