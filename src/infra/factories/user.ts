61a039184a3e16339fc0263195e47e64e500d6b0
Renato Vicente <rntvicente@gmail.com>
2023-03-12 19:02:09 -0300

    feat> criação de fabrica

---
+++ src/infra/factories/user.ts
@@ -0,0 +1,14 @@
+import { CreateUser } from '../../application/use-cases/create-user';
+
+import { MongoHelper } from '../database/helper-mongodb';
+import { UserControllerAdapter } from '../controller/user-controller-adapter';
+import { BcryptHasher } from '../encrypter/bcrypt-hasher';
+import { UserRepositoryDatabase } from '../repository/user-repository-database';
+
+export const makeCreateUserController = (): UserControllerAdapter => {
+  const database = new MongoHelper();
+  const bcrypterHasher = new BcryptHasher(12);
+  const userRepository = new UserRepositoryDatabase(database);
+  const createUser = new CreateUser(userRepository, bcrypterHasher);
+  return new UserControllerAdapter(createUser);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,17 @@
 import { MongoHelper } from './infra/database/helper-mongodb';
 import { ExpressAdapter } from './infra/server/express-adapter';
 import { UserRoute } from './infra/routes/user-router';
-import { UserControllerAdapter } from './infra/controller/user-controller-adapter';
-import { UserRepositoryDatabase } from './infra/repository/user-repository-database';
-import { BcryptHasher } from './infra/encrypter/bcrypt-hasher';
-
-import { CreateUser } from './application/use-cases/create-user';
+import { makeCreateUserController } from './infra/factories/user';
 
 const database = new MongoHelper();
 const app = new ExpressAdapter();
-
-const bcrypterHasher = new BcryptHasher(12);
-const userRepository = new UserRepositoryDatabase(database);
-const createUser = new CreateUser(userRepository, bcrypterHasher);
-const userControllerAdapter = new UserControllerAdapter(createUser);
 
 database
   .connect('mongodb://localhost:27017/ExpensesManager')
   .then(() => {
     console.info('Database connected.');
 
-    new UserRoute(app, userControllerAdapter);
+    new UserRoute(app, makeCreateUserController());
     app.listen(3000);
   })
   .catch((error) => {
