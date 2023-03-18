f9d491976977d6e0bc670af80ce5a2be81ad7cd4
Renato Vicente <rntvicente@gmail.com>
2023-03-18 17:41:32 -0300

    feat: use case

--- src/auth/application/use-cases/login.ts
+++ src/auth/application/use-cases/login.ts
@@ -1,25 +1,35 @@
-import { User } from '../../domains/users/user-entity';
+import { PasswordVO } from '../../../shared/value-object/password-vo';
+import { UserMapper } from '../../../infra/mappers/user-mapper';
+
 import { UserRepository } from '../repositories/user-repository';
+import { GeneratorToken } from '../../domains/tokens/generator-token';
 
 export class Login {
+  private readonly _now = Date.now();
+  private readonly _expiresIn = Math.floor(this._now / 1000) + 10 * 60;
+
   constructor(private readonly repository: UserRepository) {}
 
-  async execute(email: string, password: string): Promise<string> {
+  async execute(email: string, password: string): Promise<Output> {
     const storedUser = await this.repository.findOne({ email });
 
     if (!storedUser) throw new Error(`Not found user by filter ${email}.`);
 
-    const user = await User.create(
-      storedUser.firstName,
-      storedUser.lastName,
-      storedUser.email,
-      storedUser.password
-    );
-
-    const isValid = await user.validatePassword(password);
+    const passwordVO = new PasswordVO(storedUser.password);
+    const isValid = await passwordVO.validatePassword(password);
 
     if (!isValid) throw new Error('E-mail or Password invalid.');
 
-    return '';
+    const generateToken = new GeneratorToken(process.env.TOKEN_KEY);
+
+    const user = UserMapper.toDomain(storedUser);
+
+    return {
+      token: generateToken.generate(user, this._expiresIn, new Date(this._now))
+    };
   }
 }
+
+type Output = {
+  token: string;
+};
