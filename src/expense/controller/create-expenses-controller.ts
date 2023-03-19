d2e9458502eea00841832e929e4050a30306f32d
Renato Vicente <rntvicente@gmail.com>
2023-03-19 19:20:30 -0300

    feat: criação

---
+++ src/expense/controller/create-expenses-controller.ts
@@ -0,0 +1,14 @@
+import { HttpRequest, HttpResponse } from '../../infra/server/http';
+import { Controller } from '../../infra/controller/handle';
+
+import { CreateExpenses } from '../application/use-case/create-expense';
+
+export class AddExpensesControllerAdapter implements Controller {
+  constructor(private readonly createExpenses: CreateExpenses) {}
+
+  async handle({ body }: HttpRequest): Promise<HttpResponse> {
+    const expensesId = await this.createExpenses.execute(body);
+
+    return { statusCode: 201, body: expensesId };
+  }
+}
