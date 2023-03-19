ceb4952669eb0ebacf8738db4d7261296bd91f31
Renato Vicente <rntvicente@gmail.com>
2023-03-19 19:21:35 -0300

    feat: criação

---
+++ src/expense/routes/create-expenses-routes.ts
@@ -0,0 +1,15 @@
+import { Request } from 'express';
+
+import { HttpServer } from '../../infra/server/http-server';
+import { Controller } from '../../infra/controller/handle';
+
+export class CreateExpenseRoute {
+  constructor(
+    readonly httpServer: HttpServer,
+    readonly controller: Controller
+  ) {
+    httpServer.on('post', '/expenses/add', async function ({ body }: Request) {
+      return await controller.handle({ body });
+    });
+  }
+}
