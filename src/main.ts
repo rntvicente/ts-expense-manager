ef9b812c501c5e2347664c58058021c3e7a79a47
Renato Vicente <rntvicente@gmail.com>
2023-03-18 21:59:28 -0300

    feat: route /login

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import * as dotenv from 'dotenv';
 
 import { MongoHelper } from './infra/database/helper-mongodb';
+import { makeSignInController } from './infra/factories/signin-factory';
+import { makeLoginController } from './infra/factories/login-factory';
+
 import { ExpressAdapter } from './infra/server/express-adapter';
 import { UserRoute } from './auth/routes/signup-router';
-import { makeCreateUserController } from './infra/factories/user';
+import { LoginRoute } from './auth/routes/login-router';
 
 const database = new MongoHelper();
 const app = new ExpressAdapter();
@@ -15,7 +18,8 @@ database
   .then(() => {
     console.info('Database connected.');
 
-    new UserRoute(app, makeCreateUserController(database));
+    new UserRoute(app, makeSignInController(database));
+    new LoginRoute(app, makeLoginController(database));
     app.listen(process.env.PORT);
   })
   .catch((error) => {
