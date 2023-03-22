baacfec93d3932559da12169db0bcbfdb394dec1
Renato Vicente <rntvicente@gmail.com>
2023-03-21 21:31:30 -0300

    refactor: renomeando

--- src/infra/database/database-helper.ts
+++ src/infra/database/database-helper.ts
@@ -1,7 +1,8 @@
 import { Collection } from 'mongodb';
+import { Model } from './model';
 
 export interface DatabaseHelper {
   connect(url: string): Promise<void>;
   close(): Promise<void>;
-  getCollection(name: string): Promise<Collection>;
+  getCollection<m extends Model>(name: string): Promise<Collection<m>>;
 }
